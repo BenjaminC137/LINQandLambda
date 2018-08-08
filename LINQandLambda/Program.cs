688637e5f59773478352bf6fd6ee5c176afed111
benjaminc137 <benjamin.salasek@icloud.com>
2018-08-08 10:35:32 -0500

    got names to display

--- LINQandLambda/Program.cs
+++ LINQandLambda/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using SampleDataCore;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace LINQandLambda
 {
@@ -6,7 +9,30 @@ namespace LINQandLambda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //IEnumerable<Donation> names = Donation.Where(p => p.Make == "BMW" && p.Year == 2010);
+
+            IEnumerable<Donation> names = Donation.Data;
+
+            foreach (var name in names)
+            {
+
+                Console.WriteLine(name.FirstName);
+            }
+
+
+            //var names = from IEnumerable<Donation> FirstName in Donation.Data where Donation.FirstName ;
+
+
+            //List<Donation> myList = new List<Donation>();
+
+            ////var names = Donation.Data.ToString();
+
+            //foreach (var name in myList){
+                //Console.WriteLine(name);
+            }
         }
     }
-}
+
+ 
+//S=18
+//N=18
